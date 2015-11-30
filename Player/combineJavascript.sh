0e13c34d6f73ae2b51669ab1b99a7c0894d717e8
Andreas Siegrist <andreas.siegrist@gmail.com>
2015-11-30 21:07:07 +0100

    compress js

--- Player/combineJavascript.sh
+++ Player/combineJavascript.sh
@@ -1,1 +1,3 @@
-cat Decoder.js WebGLCanvas.js Player.js stream.js mp4.js >> broadwayPlayer.js
+cat Decoder.js WebGLCanvas.js Player.js stream.js mp4.js > broadwayPlayer.js
+
+uglifyjs --compress -- Decoder.js WebGLCanvas.js Player.js stream.js mp4.js > broadwayPlayer-min.js
